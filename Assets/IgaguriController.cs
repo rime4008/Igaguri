b1ebb6c957d511fa4ef4e518d13299d340f6caf6
rime4008 <rime4008@gmail.com>
2022-12-07 08:58:37 +0900

    add:IgaguriController
    イガグリを飛ばすスクリプトの追加

---
+++ Assets/IgaguriController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// ˆø”‚Åw’è‚µ‚½•ûŒü‚Ö—Í‚ğ‰Á‚¦‚é
+    /// </summary>
+    /// <param name="dir"></param>
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+}
